ca4a72df9a5c6d0420d2976f0001457e146df256
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2010-11-17 10:54:25 -0800

    Refactored the SNES ROM parsing code

--- dinocity.py
+++ dinocity.py
@@ -7,67 +7,24 @@ __author__ = ('David Anderson <dave@natulte.net>',
               'Maxime Petazzoni <maxime.petazzoni@bulix.org>')
 
 import clutter
+import logging
 import os
 import struct
+import sys
+
+import rom
+
+LOG = logging.getLogger('dinocity')
 
 # Let's pre-compute some Clutter colors
-COLOR_WHITE = clutter.color_from_string('white')
-COLOR_BLACK = clutter.color_from_string('black')
-COLOR_BLUE = clutter.color_from_string('blue')
-COLOR_RED = clutter.color_from_string('red')
+_CLUTTER_COLOR_WHITE = clutter.color_from_string('white')
+_CLUTTER_COLOR_BLACK = clutter.color_from_string('black')
+_CLUTTER_COLOR_BLUE = clutter.color_from_string('blue')
+_CLUTTER_COLOR_RED = clutter.color_from_string('red')
 
-# SMC ROM files may have an additional 512-byte SMC header at the beginning:
-#   offset  size in bytes    contents
-#  ----------------------------------------------------------------------------
-#   0       2                ROM dump size, in units of 8kB (little-endian).
-#   2       1                Binary flags for the ROM layout and save-RAM size.
-#   3       509              All zero.
-
-SMC_HEADER_SIZE = 512
-SMC_ROM_LAYOUT_LOROM = 0x00
-SMC_ROM_LAYOUT_HIROM = 0x30
-SMC_SAVERAM_SIZE_32KB = 0x00
-SMC_SAVERAM_SIZE_8KB = 0x04
-SMC_SAVERAM_SIZE_4KB = 0x08
-SMC_SAVERAM_SIZE_0kB = 0x0c
-
-# SNES ROM headers are located at addresses 0x7fc0 for LoROM images and 0xffc0
-# for HiROM images. These values may need to be offseted by 512 bytes when a
-# SMC ROM header is present (respectively 0x81c0 and 0x101c0).
-# See http://romhack.wikia.com/wiki/SNES_header for more information on SNES
-# header fields and their values.
-
-SNES_HEADER_SIZE = 64
-SNES_HEADER_OFFSET_LOROM = 0x7fc0
-SNES_HEADER_OFFSET_HIROM = 0xffc0
-
-# SNES header format:
-#   offset  size in bytes    contents
-#  ----------------------------------------------------------------------------
-#   0       21               Space-padded ASCII game title.
-#   21      1                ROM layout (LoROM / HiROM / FastROM).
-#   22      1                Cartridge type (ROM-only / with save-RAM).
-#   23      1                ROM byte size.
-#   24      1                RAM byte size.
-#   ... more, unparsed now.
-SNES_HEADER_FORMAT = '@21sBBBB'
-
-SNES_ROM_LAYOUT_LOROM = 0x20
-SNES_ROM_LAYOUT_HIROM = 0x21
-SNES_ROM_LAYOUT_FASTROM = 0x10
-
-SNES_CARTRIDGE_TYPE_ROM_ONLY = 0x00
-SNES_CARTRIDGE_TYPE_SAVERAM = 0x02
-
-
-class InvalidRomFileException(Exception):
-    """The provided ROM file is invalid or not understood by DinoCity."""
-    pass
-
-class InvalidHeaderFormatException(Exception):
-    """The SNES ROM header could not be parsed in this ROM."""
-    pass
-
+_CLUTTER_FONT_TITLE = 'Helvetica Bold 36'
+_CLUTTER_FONT_GAME_TITLE = 'Helvetica Bold 24'
+_CLUTTER_FONT_GAME_INFO = 'FreeMono 12'
 
 class Game:
     """
@@ -83,65 +40,7 @@ class Game:
                 clutter.Texture(filename=coverart),
                 self.GAME_BBOX_SIZE)
 
-        self.rom_info = self._parse_rom()
-        print self.rom_info
-
-    def _read_header_at(self, f, offset, has_smc_header=True):
-        """Read and unpack the SNES header at the given offset, eventually
-        taking into account the presence of an SMC header.
-        """
-        print ('Reading SNES header at offset %s (SMC header: %s)...' %
-               (hex(offset), bool(has_smc_header)))
-
-        try:
-            f.seek(offset + has_smc_header*SMC_HEADER_SIZE)
-            header = f.read(SNES_HEADER_SIZE)[:25]
-            data = struct.unpack(SNES_HEADER_FORMAT, header)
-            print 'Done:', data
-            return data
-        except struct.error:
-            raise InvalidHeaderFormatException
-
-        return None
-
-    def _parse_rom(self):
-        """Parses the ROM image to extract information from the SNES header
-        (game title, ROM details, etc.).
-        
-        Returns a dictionary of information."""
-
-        rom_info = {}
-        with open(self.filename) as f:
-            rom_info['size'] = os.fstat(f.fileno()).st_size
-            rem = rom_info['size'] % 1024
-            if rem == 0:
-                rom_info['has_smc_header'] = False
-            elif rem == 512:
-                rom_info['has_smc_header'] = True
-            else:
-                raise InvalidRomFileException
-
-            try:
-                header_data = self._read_header_at(f, SNES_HEADER_OFFSET_LOROM,
-                                                   rom_info['has_smc_header'])
-            except InvalidHeaderFormatException:
-                print 'Header does not match at LoROM offset.'
-                print 'Trying at HiROM offset...'
-                try:
-                    header_data = self._read_header_at(f, SNES_HEADER_OFFSET_HIROM,
-                                                       rom_info['has_smc_header'])
-                except InvalidHeaderFormatException:
-                    print 'Still no match at HiROM offset. Giving up.'
-                    raise InvalidRomFileException
-
-            rom_info['name'] = header_data[0].strip().title()
-            rom_info['layout'] = header_data[1]
-            rom_info['cartridge_type'] = header_data[2]
-            rom_info['rom_size'] = header_data[3]
-            rom_info['ram_size'] = header_data[4]
-
-        return rom_info
-
+        self.rom = rom.SNESRom(filename)
 
     def _adjust_cover_size(self, texture, bbox):
         """Adjust the game cover image to the given bounding box dimensions,
@@ -157,62 +56,36 @@ class Game:
 
         return texture
 
-    def get_layout_type(self):
-        layout = self.rom_info.get('layout', None)
-        if layout == SNES_ROM_LAYOUT_LOROM:
-            return 'LoROM'
-        elif layout == SNES_ROM_LAYOUT_HIROM:
-            return 'HiROM'
-        elif layout == SNES_ROM_LAYOUT_FASTROM:
-            return 'FastROM'
-        return 'n/a'
-
-    def get_cartridge_type(self):
-        cartridge_type = self.rom_info.get('cartridge_type', None)
-        if cartridge_type == SNES_CARTRIDGE_TYPE_ROM_ONLY:
-            return 'ROM-only'
-        elif cartridge_type == SNES_CARTRIDGE_TYPE_SAVERAM:
-            return 'save-RAM'
-        return 'n/a'
-
-    def get_info_string(self):
-        info = '%dkB %s' % (2**self.rom_info['rom_size'],
-                            self.get_layout_type())
-        if self.rom_info.get('cartridge_type', None) == SNES_CARTRIDGE_TYPE_SAVERAM:
-            info += ', with %dkB %s' % (2**self.rom_info['ram_size'],
-                                        self.get_cartridge_type())
-        else:
-            info += ', %s' % self.get_cartridge_type()
-
-        return info
-
     def render(self):
+        self.rom.parse()
+
         bbox = clutter.Group()
 
         box = clutter.Rectangle()
         box.set_position(-2,-2)
         box.set_size(self.GAME_BBOX_SIZE[0]+4, self.GAME_BBOX_SIZE[1]+4)
-        box.set_color(COLOR_BLACK)
-        box.set_border_color(COLOR_RED)
+        box.set_color(_CLUTTER_COLOR_BLACK)
+        box.set_border_color(_CLUTTER_COLOR_RED)
         box.set_border_width(2)
         bbox.add(box)
 
         name = clutter.Text()
-        name.set_text(self.rom_info.get('name', 'Unknown'))
-        name.set_position(self.GAME_BBOX_SIZE[0]+42, 0)
-        name.set_color(COLOR_WHITE)
-        name.set_font_name('Helvetica Bold 24')
+        name.set_text(self.rom.title)
+        name.set_position(0, self.GAME_BBOX_SIZE[1] + 15)
+        name.set_color(_CLUTTER_COLOR_WHITE)
+        name.set_font_name(_CLUTTER_FONT_GAME_TITLE)
         bbox.add(name)
 
         info = clutter.Text()
-        info.set_text('%s (%s)' % (self.filename, self.get_info_string()))
-        info.set_position(self.GAME_BBOX_SIZE[0]+42, 50)
-        info.set_color(COLOR_WHITE)
-        info.set_font_name('Bitstream Vera Sans Mono Roman 10')
+        info.set_text('%s (%s)' % (self.filename, self.rom.get_info_string()))
+        info.set_position(0, self.GAME_BBOX_SIZE[1] + 50)
+        info.set_color(_CLUTTER_COLOR_WHITE)
+        info.set_font_name(_CLUTTER_FONT_GAME_INFO)
         bbox.add(info)
 
         bbox.add(self.cover)
         return bbox
+
 
 class DinoCity:
 
@@ -225,22 +98,22 @@ class DinoCity:
         stage = clutter.Stage()
         stage.set_size(*screen_size)
         stage.connect('destroy', clutter.main_quit)
-        stage.set_color(COLOR_BLACK)
+        stage.set_color(_CLUTTER_COLOR_BLACK)
         stage.connect('key-press-event', self._on_key_event)
 
+        logo = clutter.Texture(filename='reptar.png')
+        logo.set_size(64, 64)
+        logo.set_position(20, 20)
+        stage.add(logo)
+
         title = clutter.Text()
-        title.set_text('DinoCity')
-        title.set_position(50, 20)
-        title.set_color(COLOR_WHITE)
-        title.set_font_name('Helvecita Bold 36')
+        title.set_text('DinoCity ROM launcher')
+        title.set_position(100, 30)
+        title.set_color(_CLUTTER_COLOR_WHITE)
+        title.set_font_name(_CLUTTER_FONT_TITLE)
         stage.add(title)
 
-        # Testing
-        game = Game('roms/simcity.smc', 'covers/simcity.jpg')
-        bbox = game.render()
-        bbox.set_position(50, 120)
-        stage.add(bbox)
-
+        LOG.debug('Created main Clutter stage for DinoCity.')
         return stage
 
     def _on_key_event(self, stage, event):
@@ -248,9 +121,21 @@ class DinoCity:
             clutter.main_quit()
 
     def go(self):
+        # Testing
+        LOG.info('Loading game roms/simcity.smc...')
+        game = Game('roms/simcity.smc', 'covers/simcity.jpg')
+        bbox = game.render()
+        bbox.set_position((self.stage.get_size()[0] -
+                           Game.GAME_BBOX_SIZE[0]) / 2,
+                          (self.stage.get_size()[1] -
+                           Game.GAME_BBOX_SIZE[1]) / 2)
+        self.stage.add(bbox)
+
+#        self.stage.set_fullscreen(True)
         self.stage.show_all()
         clutter.main()
 
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     d = DinoCity((1280, 720), 'roms/', 'covers/')
     d.go()
