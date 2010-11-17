f648c66f0531479a7c13b1e709238a24b239b109
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2010-11-17 11:35:38 -0800

    Handle left/right keys to switch between ROMs

--- dinocity.py
+++ dinocity.py
@@ -23,6 +23,7 @@ _CLUTTER_COLOR_BLUE = clutter.color_from_string('blue')
 _CLUTTER_COLOR_RED = clutter.color_from_string('red')
 
 _CLUTTER_FONT_TITLE = 'Helvetica Bold 36'
+_CLUTTER_FONT_INFO = 'Helvetica 11'
 _CLUTTER_FONT_GAME_TITLE = 'Helvetica Bold 24'
 _CLUTTER_FONT_GAME_INFO = 'FreeMono 12'
 
@@ -36,8 +37,9 @@ class Game:
 
     def __init__(self, filename, coverart='covers/_missing.png'):
         self.filename = filename
+        self.coverart = coverart
         self.cover = self._adjust_cover_size(
-                clutter.Texture(filename=coverart),
+                clutter.Texture(filename=self.coverart),
                 self.GAME_BBOX_SIZE)
 
         self.rom = rom.SNESRom(filename)
@@ -55,6 +57,9 @@ class Game:
             texture.set_position(w/hr/2, 0)
 
         return texture
+
+    def __str__(self):
+        return '<Game: %s (%s)>' % (self.filename, self.coverart)
 
     def render(self):
         self.rom.parse()
@@ -90,9 +95,17 @@ class Game:
 class DinoCity:
 
     def __init__(self, screen_size, rom_directory, cover_directory):
-        self.stage = self._create_stage(screen_size)
         self.rom_directory = rom_directory
         self.cover_directory = cover_directory
+
+        self.roms = sorted(filter(lambda x: x.endswith('.smc'),
+                                  os.listdir(self.rom_directory)))
+        LOG.info('Found %d ROMs in directory %s.' % (len(self.roms),
+                                                     self.rom_directory))
+
+        self.stage = self._create_stage(screen_size)
+        self.current_game = None
+        self.current_bbox = None
 
     def _create_stage(self, screen_size):
         stage = clutter.Stage()
@@ -108,10 +121,22 @@ class DinoCity:
 
         title = clutter.Text()
         title.set_text('DinoCity ROM launcher')
-        title.set_position(100, 30)
+        title.set_position(100, 15)
         title.set_color(_CLUTTER_COLOR_WHITE)
         title.set_font_name(_CLUTTER_FONT_TITLE)
         stage.add(title)
+
+        info = clutter.Text()
+        if not len(self.roms):
+            info.set_text('No ROM found!')
+        elif len(self.roms) == 1:
+            info.set_text('1 ROM found')
+        else:
+            info.set_text('%d ROMs found' % len(self.roms))
+        info.set_position(100, 65)
+        info.set_color(_CLUTTER_COLOR_WHITE)
+        info.set_font_name(_CLUTTER_FONT_INFO)
+        stage.add(info)
 
         LOG.debug('Created main Clutter stage for DinoCity.')
         return stage
@@ -119,19 +144,60 @@ class DinoCity:
     def _on_key_event(self, stage, event):
         if event.keyval == clutter.keysyms.Escape:
             clutter.main_quit()
+        elif event.keyval == clutter.keysyms.Right:
+            self.next_game()
+        elif event.keyval == clutter.keysyms.Left:
+            self.prev_game()
+
+    def next_game(self):
+        if not len(self.roms):
+            return
+
+        new_current_game = self.current_game + 1
+        if new_current_game >= len(self.roms):
+            new_current_game = 0
+        if new_current_game == self.current_game:
+            return
+        self.current_game = new_current_game
+        self.display_current_game()
+
+    def prev_game(self):
+        if not len(self.roms):
+            return
+
+        new_current_game = self.current_game - 1
+        if new_current_game < 0:
+            new_current_game = len(self.roms) - 1
+        if new_current_game == self.current_game:
+            return
+        self.current_game = new_current_game
+        self.display_current_game()
+
+    def display_current_game(self):
+        if self.current_bbox:
+            self.stage.remove(self.current_bbox)
+
+        game = Game(os.path.join(self.rom_directory,
+                                 self.roms[self.current_game]),
+                    os.path.join(self.cover_directory,
+                                 '%s.jpg' % self.roms[self.current_game][:-4]))
+        LOG.debug('Loaded %s' % game)
+
+        self.current_bbox = game.render()
+        self._front_and_center(self.current_bbox, yoff=42)
+        self.stage.add(self.current_bbox)
+
+    def _front_and_center(self, obj, xoff=0, yoff=0):
+        ssize = self.stage.get_size()
+        osize = obj.get_size()
+        obj.set_anchor_point(osize[0] / 2, osize[1] / 2)
+        obj.set_position(ssize[0] / 2 + xoff, ssize[1] / 2 + yoff)
 
     def go(self):
-        # Testing
-        LOG.info('Loading game roms/simcity.smc...')
-        game = Game('roms/simcity.smc', 'covers/simcity.jpg')
-        bbox = game.render()
-        bbox.set_position((self.stage.get_size()[0] -
-                           Game.GAME_BBOX_SIZE[0]) / 2,
-                          (self.stage.get_size()[1] -
-                           Game.GAME_BBOX_SIZE[1]) / 2)
-        self.stage.add(bbox)
+        if len(self.roms):
+            self.current_game = 0
+            self.display_current_game()
 
-#        self.stage.set_fullscreen(True)
         self.stage.show_all()
         clutter.main()
 
